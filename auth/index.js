c1806ff7f2446845de2ac99caaa51580e5c03a2d
vudiep411 <vudiep411@gmail.com>
2024-03-11 23:03:17 -0700

    trying to deploy to ecs

--- auth/index.js
+++ auth/index.js
@@ -15,8 +15,8 @@ import auth from "./middleware/auth.js"
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT
-const devUrl = process.env.DEV == 1 ? 'http://localhost:5001/docs/' : 'https://parking-reservation.vercel.app/docs/'
+const PORT = process.env.PORT || 5001
+const devUrl = 'http://localhost:5001/docs/'
 
 app.set('title', 'Auth API')
 app.use(bodyParser.json({ limit: "50mb", extended: true}))
